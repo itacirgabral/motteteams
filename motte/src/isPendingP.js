1bcb449c53488fe8656b1ce470019eeef8257e83
Itacir Gabral <itacirgabral@gmail.com>
2021-01-26 15:46:44 -0400

    clean unused files

--- motte/src/isPendingP.js
+++
@@ -1,9 +0,0 @@
-const uniqueVal = Promise.resolve(Symbol('uniqueVal'))
-
-const isPending = async p => {
-  const first = await Promise.race([p, uniqueVal])
-
-  return first === uniqueVal
-}
-
-module.exports = isPending
