21e35d948e3c445af6dc5121d4ce6076d0527601
Itacir Gabral <itacirgabral@gmail.com>
2021-02-19 15:33:50 -0400

    GMAPI-69

--- motte/src/punkDrummer/index.js
+++ motte/src/punkDrummer/index.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const sortingMessages = require('./sortingMessages')
+
 const punkDrummer = (seed) => {
   const keys = {
     panoptickey: 'zap:panoptic',
@@ -14,8 +17,7 @@ const punkDrummer = (seed) => {
   seed.redisB.on('message', async (channel, message) => {
     if (healthcare.playing) {
       const wbi = JSON.parse(message)
-
-
+      const wid = wbi.key.id
       const fromMe = wbi.key.fromMe
       const remoteJid = wbi.key.remoteJid.split('@s.whatsapp.net')[0]
       const participant = wbi.participant
@@ -39,73 +41,155 @@ const punkDrummer = (seed) => {
 
       const timestamp = wbi.messageTimestamp
 
-      console.dir({ from, fromMe, to, isGroup, groupId, timestamp })
+      if (!(from === 'status@broadcast' || isGroup)) {
+        const conversation = wbi.message.conversation
+        const quoteMsg = wbi.message.extendedTextMessage
+        const location = wbi.message.locationMessage
+        const contact = wbi.message.contactMessage
+        const image = wbi.message.imageMessage
+        const document = wbi.message.documentMessage
+        const audio = wbi.message.audioMessage
+        const { type, isForwarded, isQuoted, msg } = sortingMessages({
+          conversation,
+          quoteMsg,
+          location,
+          contact,
+          image,
+          document,
+          audio
+        })
 
-      let msg = wbi.message.conversation
-      const location = wbi.message.locationMessage
-      const contact = wbi.message.contactMessage
-      const image = wbi.message.imageMessage
-      const document = wbi.message.documentMessage
-      const audio = wbi.message.audioMessage
-      const quoteMsg = wbi.message.extendedTextMessage
+        console.dir({ from, fromMe, to, isGroup, groupId, timestamp, type, isForwarded, isQuoted, msg })
 
-      let type
-      let isQuoted = false
-      let isForwarded = false
-      if (msg) {
-        type = 'textMessage'
-      } else if (quoteMsg) {
-        type = 'textMessage'
-        msg = quoteMsg.text
-        if (quoteMsg.contextInfo?.isForwarded) {
-          isForwarded = true
-        }
-        if (quoteMsg.contextInfo?.stanzaId) {
-          isQuoted = true
-        }
-      } else if (contact) {
-        type = 'contactMessage'
-        if (contact?.contextInfo?.isForwarded) {
-          isForwarded = true
-        }
-        if (contact?.contextInfo?.stanzaId) {
-          isQuoted = true
-        }
-      } else if (location) {
-        type = 'locationMessage'
-        if (location?.contextInfo?.isForwarded) {
-          isForwarded = true
-        }
-        if (location?.contextInfo?.stanzaId) {
-          isQuoted = true
-        }
-      } else if (image) {
-        type = 'imageMessage'
-        if (image?.contextInfo?.isForwarded) {
-          isForwarded = true
-        }
-        if (image?.contextInfo?.stanzaId) {
-          isQuoted = true
-        }
-      } else if (document) {
-        type = 'documentMessage'
-        if (document?.contextInfo?.isForwarded) {
-          isForwarded = true
-        }
-        if (document?.contextInfo?.stanzaId) {
-          isQuoted = true
-        }
-      } else if (audio) {
-        type = 'audioMessage'
-        if (audio?.contextInfo?.isForwarded) {
-          isForwarded = true
-        }
-        if (audio?.contextInfo?.stanzaId) {
-          isQuoted = true
+        let jsontosend
+        let params
+        let file
+
+        switch (type) {
+          case 'textMessage':
+            jsontosend = {
+              type,
+              timestamp,
+              to,
+              from,
+              msg,
+              forwarded: isForwarded ? true : undefined,
+              quoted: isQuoted ? quoteMsg.contextInfo.stanzaId : undefined,
+              wid
+            }
+            await seed.redis.publish(keys.panoptickey, JSON.stringify({
+              type: 'sendhook',
+              hardid: seed.hardid,
+              shard: seed.shard,
+              json: JSON.stringify(jsontosend)
+            }))
+            break
+          case 'locationMessage':
+            jsontosend = {
+              type,
+              timestamp,
+              to,
+              from,
+              forwarded: isForwarded ? true : undefined,
+              quoted: isQuoted ? location.contextInfo.stanzaId : undefined,
+              wid,
+              description: location.address,
+              latitude: location.degreesLatitude,
+              longitude: location.degreesLongitude
+            }
+            await seed.redis.publish(keys.panoptickey, JSON.stringify({
+              type: 'sendhook',
+              hardid: seed.hardid,
+              shard: seed.shard,
+              json: JSON.stringify(jsontosend)
+            }))
+            break
+          case 'contactMessage':
+            jsontosend = {
+              type,
+              timestamp,
+              to,
+              from,
+              forwarded: isForwarded ? true : undefined,
+              quoted: isQuoted ? contact.contextInfo.stanzaId : undefined,
+              vcard: contact.vcard,
+              wid
+            }
+            await seed.redis.publish(keys.panoptickey, JSON.stringify({
+              type: 'sendhook',
+              hardid: seed.hardid,
+              shard: seed.shard,
+              json: JSON.stringify(jsontosend)
+            }))
+            break
+          case 'imageMessage':
+            file = await seed.conn.downloadAndSaveMediaMessage(wbi, path.join(process.cwd(), process.env.UPLOADFOLDER, String(Date.now())))
+            params = {
+              type,
+              timestamp,
+              to,
+              from,
+              wid,
+              caption: image.caption,
+              forwarded: isForwarded ? true : undefined,
+              quoted: isQuoted ? image.contextInfo.stanzaId : undefined,
+              mimetype: image.mimetype,
+              size: image.fileLength
+            }
+            await seed.redis.publish(keys.panoptickey, JSON.stringify({
+              type: 'sendhook',
+              hardid: seed.hardid,
+              shard: seed.shard,
+              file,
+              params
+            }))
+            break
+          case 'documentMessage':
+            file = await seed.conn.downloadAndSaveMediaMessage(wbi, path.join(process.cwd(), process.env.UPLOADFOLDER, String(Date.now())))
+            params = {
+              type,
+              timestamp,
+              to,
+              from,
+              wid,
+              forwarded: isForwarded ? true : undefined,
+              quoted: isQuoted ? document.contextInfo.stanzaId : undefined,
+              filename: document.fileName,
+              mimetype: document.mimetype,
+              size: document.fileLength
+            }
+            await seed.redis.publish(keys.panoptickey, JSON.stringify({
+              type: 'sendhook',
+              hardid: seed.hardid,
+              shard: seed.shard,
+              file,
+              params
+            }))
+            break
+          case 'audioMessage':
+            file = await seed.conn.downloadAndSaveMediaMessage(wbi, path.join(process.cwd(), process.env.UPLOADFOLDER, String(Date.now())))
+            params = {
+              type,
+              timestamp,
+              to,
+              from,
+              wid,
+              forwarded: isForwarded ? true : undefined,
+              quoted: isQuoted ? audio.contextInfo.stanzaId : undefined,
+              seconds: audio.seconds,
+              mimetype: audio.mimetype,
+              size: audio.fileLength
+            }
+            await seed.redis.publish(keys.panoptickey, JSON.stringify({
+              type: 'sendhook',
+              hardid: seed.hardid,
+              shard: seed.shard,
+              file,
+              params
+            }))
+            break
         }
       }
-
-      console.dir({ type, isForwarded, isQuoted })
     } else {
       seed.redisB.unsubscribe(keys.spreadKey)
     }
