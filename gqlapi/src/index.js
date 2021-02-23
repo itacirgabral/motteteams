cc0a652d00668eff933935c6608a4a1c0f5656b4
Itacir Gabral <itacirgabral@gmail.com>
2021-02-23 15:39:01 -0400

    GMAPI-81 WIP verify is member

--- gqlapi/src/index.js
+++ gqlapi/src/index.js
@@ -51,6 +51,10 @@ const server = new ApolloServer({
     onOperationComplete: () => {
       console.log('onOperationComplete')
     }
+  },
+  cors: {
+    origin: '*',
+    credentials: true
   }
 })
 
--- gqlapi/src/resolvers/mutationSendtextmessage.js
+++ gqlapi/src/resolvers/mutationSendtextmessage.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server')
+const { AuthenticationError, UserInputError } = require('apollo-server')
 
 const timeout = Number(process.env.TIMEOUT || '5000')
 
@@ -8,47 +8,60 @@ const mutationSendtextmessage = async (parent, args, context, info) => {
     const { to, msg, quote } = args.input
     const radiohookkey = `zap:${context.user.shard}:radiohook`
     const rawbreadkey = `zap:${context.user.shard}:fifo:rawBread`
+    const chatsKeys = `zap:${context.user.shard}:chats`
+    const markcountkey = `zap:${context.user.shard}:markcount`
 
-    const type = 'textMessage_v001'
-    const random = String(Math.random()).slice(2)
-    const jid = to.indexOf('-') === -1
-      ? `${to}@s.whatsapp.net` // se for pessoa
-      : `${to}@g.us` // se for grupo
+    const pipeline = context.redis.pipeline()
+    pipeline.sismember(chatsKeys, to)
+    pipeline.incr(markcountkey)
+    const pipeback = await pipeline.exec()
 
-    await redisB.subscribe(radiohookkey)
-    const queryResult = new Promise((resolve, reject) => {
-      setTimeout(() => {
-        redisB.unsubscribe(radiohookkey)
-        resolve()
-      }, timeout)
+    const alreadyTalkedTo = pipeback[0][1]
+    const mark0 = pipeback[1][1]
 
-      redisB.on('message', (channel, message) => {
-        const { type, mark, wid, timestamp, from } = JSON.parse(message)
-        if (type === 'sent' && mark === random) {
-          redisB.unsubscribe()
-          resolve({
-            wid,
-            timestamp,
-            to,
-            from,
-            msg,
-            quote,
-            forwarded: false
-          })
-        }
+    if (alreadyTalkedTo) {
+      const type = 'textMessage_v001'
+      const jid = to.indexOf('-') === -1
+        ? `${to}@s.whatsapp.net` // se for pessoa
+        : `${to}@g.us` // se for grupo
+
+      await redisB.subscribe(radiohookkey)
+      const queryResult = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          redisB.unsubscribe(radiohookkey)
+          resolve()
+        }, timeout)
+
+        redisB.on('message', (channel, message) => {
+          const { type, mark, wid, timestamp, from } = JSON.parse(message)
+          if (type === 'sent' && mark === mark0) {
+            redisB.unsubscribe()
+            resolve({
+              wid,
+              timestamp,
+              to,
+              from,
+              msg,
+              quote,
+              forwarded: false
+            })
+          }
+        })
       })
-    })
 
-    // o comando de envio de mensagem
-    await context.redis.lpush(rawbreadkey, JSON.stringify({
-      type,
-      mark: random,
-      jid,
-      quote,
-      msg
-    }))
+      // o comando de envio de mensagem
+      await context.redis.lpush(rawbreadkey, JSON.stringify({
+        type,
+        mark: mark0,
+        jid,
+        quote,
+        msg
+      }))
 
-    return queryResult
+      return queryResult
+    } else {
+      throw new UserInputError('Never seen before.')
+    }
   } else {
     throw new AuthenticationError('do auth')
   }
