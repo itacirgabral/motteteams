884c7412dcda0132dc08096b1b5a28176f8de63b
Itacir Gabral <itacirgabral@gmail.com>
2021-02-26 13:58:54 -0400

    alreadytalkedto chats

--- restapi/router/alreadytalkedto.js
+++ restapi/router/alreadytalkedto.js
@@ -1,14 +1,17 @@
 const alreadytalkedto = ({ redis }) => (req, res) => {
   const shard = req.shard
   if (req.params.number) {
-    const jid = `${req.params.number}@s.whatsapp.net`
-    redis.sismember(`zap:${shard}:contacts`, jid)
+    const number = req.params.number
+
+    redis.sismember(`zap:${shard}:chats`, number)
       .catch(() => {
         res.status(500).end()
       })
       .then(alreadytalkedto => {
         res.status(200).json({
           type: 'alreadytalkedto',
+          shard,
+          number,
           alreadytalkedto: !!alreadytalkedto
         })
       })
