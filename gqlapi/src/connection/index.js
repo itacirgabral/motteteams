f79aa671a807ae330d839d32675c2b98eaf416c6
Itacir Gabral <itacirgabral@gmail.com>
2021-02-27 16:35:27 -0400

    GMAPI-100 conn state

--- gqlapi/src/connection/index.js
+++ gqlapi/src/connection/index.js
@@ -14,7 +14,40 @@ module.exports = {
     }
   `,
   resolvers: {
-    Query: {},
+    Query: {
+      connectionstate: async (parent, args, context, info) => {
+        if (context.user) {
+          const redisB = context.redis.duplicate()
+          const radiohookkey = `zap:${context.user.shard}:radiohook`
+
+          await redisB.subscribe(radiohookkey)
+          const queryResult = new Promise((resolve, reject) => {
+            setTimeout(() => {
+              redisB.unsubscribe(radiohookkey)
+              resolve(null)
+            }, timeout)
+
+            redisB.on('message', (channel, message) => {
+              const { type, state } = JSON.parse(message)
+              if (type === 'connectionstate') {
+                redisB.unsubscribe()
+                resolve(state)
+              }
+            })
+          })
+
+          await context.redis.publish(context.panoptickey, JSON.stringify({
+            type: 'connectionstate',
+            hardid: context.hardid,
+            shard: context.user.shard
+          }))
+
+          return queryResult
+        } else {
+          throw new AuthenticationError('do auth')
+        }
+      }
+    },
     Mutation: {
       reconect: async (parent, args, context, info) => {
         if (context.user) {
