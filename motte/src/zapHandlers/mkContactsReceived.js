8b8cbeb67b7fa8067e9a11080311ea8c278f570e
Itacir Gabral <itacirgabral@gmail.com>
2021-03-18 20:48:52 -0400

    GMAPI-148

--- motte/src/zapHandlers/mkContactsReceived.js
+++ motte/src/zapHandlers/mkContactsReceived.js
@@ -6,6 +6,8 @@ const contactsReceived = (seed) => {
   const logKey = `zap:${seed.shard}:log`
   const newsKey = `zap:${seed.shard}:news`
 
+  const n = 0
+
   return async () => {
     const json = JSON.stringify({ event: 'contacts-received', data: null })
     const pipeline = seed.redis.pipeline()
@@ -13,6 +15,25 @@ const contactsReceived = (seed) => {
     pipeline.ltrim(logKey, 0, 999)
     pipeline.publish(newsKey, json)
     await pipeline.exec()
+
+    setTimeout(() => {
+      const checkin = seed.conn.chats.array.map((el, idx) => {
+        const r = {}
+        r.chat = el.jid.split('@')[0]
+        r.name = el.name
+
+        if (el.messages.array[0]?.key?.id) {
+          const wid = el.messages.array[0].key.id
+          r.wid = wid
+        }
+
+        return r
+      })
+        .filter(el => !!el.wid)
+
+      const checkinString = JSON.stringify(checkin)
+      console.log(checkinString)
+    }, 5_000)
   }
 }
 
