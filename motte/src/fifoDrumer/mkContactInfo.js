3fc384a99815bd7403dfccd51b623e454815bd77
Itacir Gabral <itacirgabral@gmail.com>
2021-05-12 16:53:59 -0400

    GMAPI-212

--- motte/src/fifoDrumer/mkContactInfo.js
+++ motte/src/fifoDrumer/mkContactInfo.js
@@ -28,6 +28,22 @@ const mkContactInfo = ({
     seed.redis.publish(panoptickey, JSON.stringify(notifysent))
 
     await delay(250 * (1 + Math.random()))
+  } else {
+    const notifysent = {
+      type: 'sendhook',
+      hardid: seed.hardid,
+      shard: seed.shard,
+      json: JSON.stringify({
+        type: 'contact',
+        from: seed.shard,
+        number: crumb.jid.split('@s.whatsapp.net')[0],
+        name: '',
+        status: '',
+        avatar: ''
+      })
+    }
+
+    seed.redis.publish(panoptickey, JSON.stringify(notifysent))
   }
 
   await seed.redis.ltrim(lastRawKey, 0, -2)
