7f762f58d2ac3363cdb7aa930fdc323c2e3007e4
Itacir Gabral <itacirgabral@gmail.com>
2021-06-13 18:23:22 -0400

    GMAPI-286

--- motte/src/sendHook.js
+++ motte/src/sendHook.js
@@ -2,26 +2,45 @@ const fetch = require('node-fetch')
 const FormData = require('form-data')
 const fs = require('fs')
 
+const method = 'POST'
+const headers = {
+  'Content-Type': 'application/json'
+}
+
 const sendHook = async ({ redis, json, shard, params, file }) => {
   const webhookkey = `zap:${shard}:webhook`
+  const webhookkey2 = `zap:${shard}:webhook2`
   const webhookhistory = `zap:${shard}:webhook:history`
   const radiohookkey = `zap:${shard}:radiohook`
 
   let tohookhistory
+  const pipeline = redis.pipeline()
+  pipeline.get(webhookkey)
+  pipeline.get(webhookkey2)
+  const [
+    [, webhook],
+    [, webhook2]
+  ] = await pipeline.exec()
+
   if (typeof json === 'string') {
-    const webhook = await redis.get(webhookkey)
-    if (webhook) {
-      await fetch(webhook, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: json
-      }).catch(() => {})
-    }
+    await Promise.all([
+      webhook
+        ? fetch(webhook, {
+            method,
+            headers,
+            body: json
+          }).catch(() => {})
+        : Promise.resolve(),
+      webhook2
+        ? fetch(webhook2, {
+            method,
+            headers,
+            body: json
+          }).catch(() => {})
+        : Promise.resolve()
+    ])
     tohookhistory = json
   } else if (typeof file === 'string') {
-    const webhook = await redis.get(webhookkey)
     if (webhook) {
       const url = new URL(webhook)
       Object.entries(params).forEach(el => {
@@ -31,9 +50,23 @@ const sendHook = async ({ redis, json, shard, params, file }) => {
       form.append('file', fs.createReadStream(file))
 
       await fetch(url.href, {
-        method: 'POST', body: form
+        method, body: form
       }).catch(() => {})
     }
+
+    if (webhook2) {
+      const url = new URL(webhook2)
+      Object.entries(params).forEach(el => {
+        url.searchParams.append(el[0], el[1])
+      })
+      const form = new FormData()
+      form.append('file', fs.createReadStream(file))
+
+      await fetch(url.href, {
+        method, body: form
+      }).catch(() => {})
+    }
+
     fs.unlinkSync(file)
     tohookhistory = JSON.stringify(params)
   }
