697786833324109bee78ad572038b16de7bcb9bc
Itacir Gabral <itacirgabral@gmail.com>
2021-10-06 10:05:44 -0400

    cleanall

--- zapql/sidebars.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  someSidebar: {
-    Apresentacao: ['doc1', 'doc2', 'doc3'],
-    "Instancias": ['mdx'],
-    "Envio de mensagens": ['mdx'],
-    "Recebimento de mensagens": ['mdx'],
-  },
-};
