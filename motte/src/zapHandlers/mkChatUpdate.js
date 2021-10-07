1e6890e765bb72a880c6045984b17f4ee51ce298
Itacir Gabral <itacirgabral@gmail.com>
2021-10-07 18:24:51 -0400

    GMAPI-501

--- motte/src/zapHandlers/mkChatUpdate.js
+++ motte/src/zapHandlers/mkChatUpdate.js
@@ -3,11 +3,14 @@
  * on (event: 'chat-update', listener: (chat: Partial<WAChat> & { jid: string }) => void): this
  */
 
+const chatprocessor = require('./chatprocessor')
+
 const retention = Number(process.env.REDIS_RETENTION_TIMESERIES_MS || '86400000')
 
 const chatUpdate = (seed) => {
   const logKey = `zap:${seed.shard}:log`
   const newsKey = `zap:${seed.shard}:news`
+  const panoptickey = 'zap:panoptic'
   const tskey = `zap:${seed.shard}:ts:event:chat-update`
 
   return async (chat) => {
@@ -18,6 +21,39 @@ const chatUpdate = (seed) => {
     pipeline.ltrim(logKey, 0, 999)
     pipeline.publish(newsKey, json)
     pipeline.call('TS.ADD', tskey, '*', 1, 'RETENTION', retention, 'LABELS', 'shard', seed.shard, 'event', 'chat-update')
+
+    //
+    const messages = chat?.messages?.all()
+    if (Array.isArray(messages) && messages.length > 0) {
+      const wbis = messages
+        .map(message => message.toJSON())
+        .map(wbi => ({
+          shard: seed.shard,
+          wbi
+        }))
+
+      const newones = wbis.map(chatprocessor)
+      newones.forEach(({ filename, newone }, idx) => {
+        if (newone) {
+          // const file = await seed.conn.downloadAndSaveMediaMessage(wbi, path.join(process.cwd(), process.env.UPLOADFOLDER, String(Date.now())))
+
+          // TODO não lembro o que é params e o que é json
+          const hook = {
+            type: 'sendhook',
+            hardid: seed.hardid,
+            shard: seed.shard,
+            file, // only media messagens have it
+            params,
+            json: JSON.stringify(newone)
+          }
+          pipeline.publish(panoptickey, JSON.stringify(hook))
+
+          // couchdb + minio
+        } else {
+          // console.dir({ chat })
+        }
+      })
+    }
 
     await pipeline.exec()
   }
