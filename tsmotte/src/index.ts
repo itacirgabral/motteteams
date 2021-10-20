f90bd2471da39336deeb5b44fd2e010a9cdd5789
Itacir Gabral <itacirgabral@gmail.com>
2021-10-20 18:03:44 -0400

    docs

--- tsmotte/src/index.ts
+++ tsmotte/src/index.ts
@@ -1,5 +1,19 @@
 import * as rediskeys from './rediskeys'
+import { ConnAdm } from './schema'
+import { Connect } from './schema/ConnAdm/Connect'
+import { Connectionstate } from './schema/ConnAdm/Connectionstate'
+import { Disconnect } from './schema/ConnAdm/Disconnect'
+import { Queuerestart } from './schema/ConnAdm/Queuerestart'
+import { Signupconnection } from './schema/ConnAdm/Signupconnection'
+import { Spreadrestart } from './schema/ConnAdm/Spreadrestart'
 
 export {
-  rediskeys
+  rediskeys,
+  ConnAdm,
+  Connect,
+  Connectionstate,
+  Disconnect,
+  Queuerestart,
+  Signupconnection,
+  Spreadrestart
 }
--- tsmotte/src/schema/index.ts
+++ tsmotte/src/schema/index.ts
@@ -1,12 +1,24 @@
 import {
-  Connect, isConnect,
-  Connectionstate, isConnectionstate,
-  Disconnect, isDisconnect,
-  Queuerestart, isQueuerestart,
-  Signupconnection, isSignupconnection,
-  Spreadrestart, isSpreadrestart
+  Connect,
+  Connectionstate,
+  Disconnect,
+  Queuerestart,
+  Signupconnection,
+  Spreadrestart,
 } from './ConnAdm'
 
+import {
+  isConnect,
+  isConnectionstate,
+  isDisconnect,
+  isQueuerestart,
+  isSignupconnection,
+  isSpreadrestart
+} from './ConnAdm'
+
+/**
+ * Tipo ConnAdm, conjunto de tipos que controla a conexão
+ */
 type ConnAdm = Connect | Connectionstate | Disconnect | Queuerestart | Signupconnection | Spreadrestart
 
 const isConnAdm = {
