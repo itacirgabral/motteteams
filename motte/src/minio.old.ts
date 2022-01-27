ec1169543d1c7b2ea90c370b6639b01577122330
Itacir Gabral <itacirgabral@gmail.com>
2022-01-27 09:02:43 -0400

    backup

---
+++ motte/src/minio.old.ts
@@ -0,0 +1,19 @@
+import Minio from 'minio'
+
+const endPoint = process.env.MINIO_END_POINT || 'localhost'
+const port = Number(process.env.MINIO_PORT) || 9001
+const useSSL = process.env.MINIO_USE_SSL === 'yes'
+const accessKey = process.env.MINIO_ACCESS_KEY || ''
+const secretKey = process.env.MINIO_SECRET_KEY || ''
+
+const minio = new Minio.Client({
+  endPoint,
+  port,
+  useSSL,
+  accessKey,
+  secretKey
+})
+
+export {
+  minio
+}
