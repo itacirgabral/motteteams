ff12a41450e3576a3267bca1353f308a7230b420
Itacir Gabral <itacirgabral@gmail.com>
2022-02-21 11:20:53 -0400

    .

--- initdb.d/putOffice.ts
+++ initdb.d/putOffice.ts
@@ -1,26 +1,24 @@
 import fs from 'fs'
-import { json2office, client, GestorSistemasOffice } from '@gmapi/redispack'
+import { client as redis, json2office, GestorSistemasOffice } from '@gmapi/redispack'
 
 require('dotenv').config({
   path: require('path').join(__dirname, '.env')
 })
 
-const hardid = process.env.HARDID
+// const hardid = process.env.HARDID
 
-const json = fs.readFileSync('./dumpup/office.json', 'utf8')
+// const json = fs.readFileSync('./dumpup/office.json', 'utf8')
 
-if (json) {
-  const obj = JSON.parse(json) as {
-    data: Array<GestorSistemasOffice>
-  }
+// if (json) {
+//   const obj = JSON.parse(json) as {
+//     data: Array<GestorSistemasOffice>
+//   }
+//   const data = obj.data
+//   const prefix = `hardid:${hardid}`
 
-  const prefix = `hardid:${hardid}`
+//   json2office({ redis, prefix, data }).then(console.dir)
+// } else {
+//   console.log('./dumpup/office.json not found')
+// }
 
-  json2office({
-    redis: client,
-    prefix,
-    data: obj.data
-  }).then(console.dir)
-} else {
-  console.log('./dumpup/office.json not found')
-}
+console.dir(redis)
